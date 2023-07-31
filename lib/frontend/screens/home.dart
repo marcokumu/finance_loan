93ea92939d95c6eb6657dbcd897553edba7b2c19
marcokumu <marcokumu10@gmail.com>
2023-07-31 16:03:58 +0300

    🔧fix: remove redundant lines of code

--- lib/frontend/screens/home.dart
+++ lib/frontend/screens/home.dart
@@ -3,12 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finance_loan/frontend/widgets/floating-action-button/floating_action_button.dart';
 import 'package:finance_loan/frontend/widgets/home-widgets/loan_data.dart';
-
-import 'package:finance_loan/frontend/widgets/home-widgets/tab_widget.dart'; // Import the TabWidget
 import 'package:finance_loan/frontend/widgets/home-widgets/tab_widget.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
   const Home({Key? key, required this.userId}) : super(key: key);
   final String userId; // The current user's ID
 
@@ -17,20 +14,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
-  // ... Other code remains the same ...
-
   late TabController _tabController;
   final List<String> _tabTitles = ['Owed To Me', 'Borrowed'];
-  final List<int> _lendAmounts = [
-    767981,
-    6578921
-  ]; // Replace these with your dynamic amounts
-  final List<int> _borrowAmounts = [
-    8765678,
-    987657
-  ]; // Replace these with your dynamic amounts
-
-  // Sample loan data. Replace this with your actual loan data list.
 
   @override
   void initState() {
@@ -44,7 +29,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   void dispose() {
     _tabController.dispose();
     super.dispose();
-    
   }
 
   void _handleTabSelection() {
@@ -55,42 +39,29 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void getCurrentUser(){
-    if(!_auth.currentUser!.isAnonymous){
   void getCurrentUser() {
     if (!_auth.currentUser!.isAnonymous) {
       final User user = _auth.currentUser!;
       userId = user.uid;
     }
   }
- 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       body: DefaultTabController(
         length: 2,
         child: Column(
           children: [
-            // Use the TabWidget here, passing the required parameters
             TabWidget(
               tabController: _tabController,
               tabTitles: _tabTitles,
-              lendAmounts: _lendAmounts,
-              borrowAmounts: _borrowAmounts,
             ),
-
-            // TransactionChart(),
-
-            // Wrap the ListView.builder with Expanded
             Expanded(
               child: TabBarView(
                 controller: _tabController,
                 children: [
-                  LoanCardsList(userId: userId, loanType: 'Lend'),
-                  LoanCardsList(userId: userId, loanType: 'Borrow'),
                   _getTabContent('Lend', userId),
                   _getTabContent('Borrow', userId),
                 ],
