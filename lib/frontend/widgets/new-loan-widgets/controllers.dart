b7bcc7aaab6a137ca1f5c779dd17906594ca5e93
marcokumu <marcokumu10@gmail.com>
2023-08-03 00:13:41 +0300

    ✨feat: text editing controllers for input fields

---
+++ lib/frontend/widgets/new-loan-widgets/controllers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+final TextEditingController fullNameController = TextEditingController();
+final TextEditingController telephoneNumberController =
+    TextEditingController();
+final TextEditingController emailController = TextEditingController();
+final TextEditingController loanAmountController = TextEditingController();
+final TextEditingController descriptionController = TextEditingController();
+
