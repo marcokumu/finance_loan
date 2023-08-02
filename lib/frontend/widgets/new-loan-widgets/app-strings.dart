a2b1937d0870a4a64218cec238b6c784d14a389d
marcokumu <marcokumu10@gmail.com>
2023-08-02 03:04:25 +0300

    ✨feat: created the strings for the form

---
+++ lib/frontend/widgets/new-loan-widgets/app-strings.dart
@@ -0,0 +1,17 @@
+
+class AppStrings {
+  static const String title = 'New Loan';
+  static const String fullName = 'Full Name';
+  static const String telephoneNumber = 'Telephone Number';
+  static const String email = 'Email';
+  static const String lend = 'Lend';
+  static const String borrow = 'Borrow';
+  static const String loanAmount = 'Loan Amount';
+  static const String description = 'Description';
+  static const String dateOfLoan = 'Date of Loan';
+  static const String paymentDueDate = 'Payment Due Date';
+  static const String save = 'Save';
+  static const String errorDialogTitle = 'Error';
+  static const String requiredFieldsError =
+      'Please fill all the required fields.';
+}
