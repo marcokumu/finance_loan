14078aa469634416166aaafd6a5bc9ecde6a3576
marcokumu <marcokumu10@gmail.com>
2023-08-07 14:49:10 +0300

    ♻️refactor: adjusted the tabs

--- lib/frontend/widgets/home-widgets/tab_widget.dart
+++ lib/frontend/widgets/home-widgets/tab_widget.dart
@@ -14,11 +14,15 @@ class TabWidget extends StatefulWidget {
   _TabWidgetState createState() => _TabWidgetState();
 }
 
-class _TabWidgetState extends State<TabWidget> {
+class _TabWidgetState extends State<TabWidget>
+    with AutomaticKeepAliveClientMixin<TabWidget> {
+  @override
+  bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
+      // padding: const EdgeInsets.symmetric(horizontal: 50),
       decoration: const BoxDecoration(
         border: Border(
           bottom: BorderSide(
@@ -26,7 +30,6 @@ class _TabWidgetState extends State<TabWidget> {
             width: 0.0,
           ),
         ),
-        color: Color.fromARGB(255, 5, 5, 5),
       ),
       child: Column(
         children: [
@@ -36,46 +39,23 @@ class _TabWidgetState extends State<TabWidget> {
               width: MediaQuery.of(context).size.width * 0.8,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(30),
-                color: Colors
-                    .grey[800], // Change the background color of the search bar
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12),
-                child: TextField(
-                  cursorColor: Colors.white,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    hintText: 'Search...',
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    border: InputBorder.none,
-                    prefixIcon:
-                        Icon(Icons.search_outlined, color: Colors.white),
-                  ),
-                ),
               ),
             ),
           ),
           TabBar(
-            // indicatorPadding: EdgeInsets.all(40),
-            // indicatorSize: TabBarIndicatorSize.label,
-            // indicatorColor: Colors.black,
             controller: widget.tabController,
-            indicator: const BoxDecoration(
-              color: Color.fromARGB(0, 49, 47, 47),
+            indicator: const UnderlineTabIndicator(
+              borderSide: BorderSide(
+                width: 1,
+              ),
+              insets: EdgeInsets.symmetric(horizontal: 16.0),
             ),
             unselectedLabelStyle: const TextStyle(
-              fontFamily: 'Roboto',
               fontSize: 14,
-              color: Color.fromARGB(255, 236, 232, 232),
             ),
             labelStyle: const TextStyle(
-              fontFamily: 'Roboto',
               fontSize: 16,
-              color: Colors.black,
             ),
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
             tabs: [
               buildTab('Owed To Me'),
               buildTab('Borrowed'),
@@ -89,15 +69,10 @@ class _TabWidgetState extends State<TabWidget> {
   Widget buildTab(String title) {
     final bool isActiveTab =
         widget.tabController.index == widget.tabTitles.indexOf(title);
-    const Color activeColor = Colors.white;
-    const Color inactiveColor =
-        Color.fromARGB(251, 241, 238, 238); // Provide a non-null default value
 
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
-        color:
-            isActiveTab ? activeColor : const Color.fromARGB(189, 88, 82, 82),
       ),
       child: Padding(
         padding: const EdgeInsets.all(1.0),
@@ -108,8 +83,6 @@ class _TabWidgetState extends State<TabWidget> {
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  // crossAxisAlignment: CrossAxisAlignment.center,
-
                   children: [
                     Text(
                       title,
@@ -118,8 +91,6 @@ class _TabWidgetState extends State<TabWidget> {
                         letterSpacing: 1,
                         fontSize: 16,
                         fontWeight: isActiveTab ? FontWeight.bold : null,
-                        fontFamily: isActiveTab ? 'Roboto-Bold' : 'Roboto',
-                        color: isActiveTab ? Colors.black : inactiveColor,
                       ),
                     ),
                   ],
