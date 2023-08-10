e4fb8bac9e2a39f39898e9417d0d6d32b9c4ad5c
marcokumu <marcokumu10@gmail.com>
2023-08-10 14:50:26 +0300

    🗑️bin: got rid of redundant files

--- lib/frontend/widgets/new-loan-widgets/controllers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final TextEditingController fullNameController = TextEditingController();
-final TextEditingController telephoneNumberController =
-    TextEditingController();
-final TextEditingController emailController = TextEditingController();
-final TextEditingController loanAmountController = TextEditingController();
-final TextEditingController descriptionController = TextEditingController();
-
