ade8c1249861b88dca66e18b35520572ba556526
marcokumu <marcokumu10@gmail.com>
2023-08-15 22:05:50 +0300

    ♻️refactor: removed unnecessary lines of code

--- lib/frontend/nav-icons/page_option.dart
+++ lib/frontend/nav-icons/page_option.dart
@@ -13,50 +13,40 @@ const List<PageOption> destinations = <PageOption>[
       'Home',
       Icon(
         Icons.home,
-        // color: Color.fromARGB(255, 90, 87, 87),
       ),
       Icon(
         Icons.home,
-        // color: Color.fromRGBO(191, 55, 33, 1),
       )),
   PageOption(
       'Transactions',
       Icon(
         Icons.wallet,
-        // color: Color.fromARGB(255, 90, 87, 87),
       ),
       Icon(
         Icons.wallet,
-        // color: Color.fromRGBO(191, 55, 33, 1),
       )),
   PageOption(
       'Inventory',
       Icon(
         Icons.text_snippet,
-        // color: Color.fromARGB(255, 90, 87, 87),
       ),
       Icon(
         Icons.text_snippet,
-        // color: Color.fromRGBO(191, 55, 33, 1),
       )),
   PageOption(
       'New Loan',
       Icon(
         Icons.local_atm,
-        // color: Color.fromARGB(255, 90, 87, 87),
       ),
       Icon(
         Icons.local_atm,
-        // color: Color.fromRGBO(191, 55, 33, 1),
       )),
   PageOption(
       'Notifications',
       Icon(
         Icons.notifications,
-        // color: Color.fromARGB(255, 90, 87, 87),
       ),
       Icon(
         Icons.notifications,
-        // color: Color.fromRGBO(191, 55, 33, 1),
       )),
 ];
