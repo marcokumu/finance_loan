3f98b67e0aa91ecc8dbf8a7d3a093e38e357c106
marcokumu <marcokumu10@gmail.com>
2023-08-16 06:37:45 +0300

    ✨feat: added change notifier for the ThemeModel to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,50 @@
 import 'package:finance_loan/frontend/nav/bottom_navigation_bar.dart';
 import 'package:finance_loan/frontend/theme/app_theme.dart';
 import 'package:finance_loan/frontend/theme/theme_model.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart'; // Import the provider package
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
-// Import the theme_switch.dart file
+
+Future<void> initializeApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Future.wait([
+    // Initialize Firebase
+    Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    ),
+  ]);
+}
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await initializeApp();
 
   runApp(
-    ChangeNotifierProvider(
-      // Wrap with ChangeNotifierProvider to provide the ThemeModel
-      create: (_) => ThemeModel(),
-      child: MyApp(),
+    MultiProvider(
+      // Use MultiProvider to provide multiple providers if needed
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeModel()),
+        // Add other providers here if needed
+      ],
+      child: const MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
-      themeMode: Provider.of<ThemeModel>(context)
-          .themeMode, // Access the themeMode from the provider
-      home: BottomNavigationBarExample(),
+      themeMode: Provider.of<ThemeModel>(context).themeMode,
+      home: const BottomNavigationBarExample(),
+      builder: EasyLoading.init(),
     );
   }
 }
-
-// ... Rest of the code
