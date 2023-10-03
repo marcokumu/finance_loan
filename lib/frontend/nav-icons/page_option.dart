3431d70df58cf686d0939189506b6026d662ff26
marcokumu <marcokumu10@gmail.com>
2023-10-03 08:37:02 +0300

    ♻️refactor: changed the bottom nav icons and general UI

--- lib/frontend/nav-icons/page_option.dart
+++ lib/frontend/nav-icons/page_option.dart
@@ -1,52 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+import '../services/darklight_mode_icon.dart';
 
 class PageOption {
-  const PageOption(this.label, this.icon, this.selectedIcon);
+  const PageOption(this.label, this.icon);
 
   final String label;
   final Widget icon;
-  final Widget selectedIcon;
 }
 
 const List<PageOption> destinations = <PageOption>[
-  PageOption(
-      'Home',
-      Icon(
-        Icons.home,
-      ),
-      Icon(
-        Icons.home,
-      )),
-  PageOption(
-      'Transactions',
-      Icon(
-        Icons.wallet,
-      ),
-      Icon(
-        Icons.wallet,
-      )),
-  PageOption(
-      'Inventory',
-      Icon(
-        Icons.text_snippet,
-      ),
-      Icon(
-        Icons.text_snippet,
-      )),
-  PageOption(
-      'New Loan',
-      Icon(
-        Icons.local_atm,
-      ),
-      Icon(
-        Icons.local_atm,
-      )),
-  PageOption(
-      'Notifications',
-      Icon(
-        Icons.notifications,
-      ),
-      Icon(
-        Icons.notifications,
-      )),
+  PageOption('Home', DarkLightModeIcon(assetPath: 'assets/icons/house-chimney.svg', width: 24, height: 24)),
+  PageOption('Trends',  DarkLightModeIcon(assetPath: 'assets/icons/stats.svg', width: 24, height: 24)),
+  PageOption('Inventory', Icon(Ionicons.receipt_outline)),
+  PageOption('New Loan', Icon(Ionicons.add_circle_outline)),
+  PageOption('Alerts', Icon(Ionicons.notifications_outline)),
 ];
